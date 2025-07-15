e5261a430f012dfd48fb0475afb9b77b1480f06f
FochesatoLuca <62165149+FochesattoLuca@users.noreply.github.com>
2025-07-15 14:28:54 -0800

    open in new terminal

--- code/Video_test.sh
+++ code/Video_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo seek_viewer --camtype=seek --rotate=180 --colormap=11 --mode=file --output=./seek.avi & sudo libcamera-vid --nopreview -t 5000 -o test.h264 &
+lxterminal & sudo seek_viewer --camtype=seek --rotate=180 --colormap=11 --mode=file --output=./seek.avi & sudo libcamera-vid --nopreview -t 5000 -o test.h264 &
 sudo ffmpeg -i test.h264 nir.avi
 
