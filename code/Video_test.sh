79552fb73d99674ed2583f6652de1c858165728a
FochesatoLuca <62165149+FochesattoLuca@users.noreply.github.com>
2025-07-15 14:22:33 -0800

    made commands run concurrently

--- code/Video_test.sh
+++ code/Video_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo seek_viewer --camtype=seek --rotate=180 --colormap=11 --mode=file --output=./seek.avi
-sudo libcamera-vid -t 5000 -o test.h264
+sudo seek_viewer --camtype=seek --rotate=180 --colormap=11 --mode=file --output=./seek.avi &
+sudo libcamera-vid -t 5000 -o test.h264 &
 sudo ffmpeg -i test.h264 nir.avi
 
